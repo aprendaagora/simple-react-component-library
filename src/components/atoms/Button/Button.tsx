2c9d12c294710f1b933185ea161cee8db85c211a
aprendaagora <aprenda.agora.contato@gmail.com>
2023-03-05 09:34:34 -0300

    tailwind implemented

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,11 +1,35 @@
 import React from "react";
+import "../../../output.css";
 
 export interface ButtonProps {
   label: string;
+  tailwind?: string;
+  type?: "primary" | "success" | "danger" | "secondary" | "light" | "dark";
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const Button = ({ label, tailwind = "", type = "primary" }: ButtonProps) => {
+  const bgColor = {
+    primary: "blue",
+    success: "green",
+    danger: "red",
+    secondary: "gray",
+    light: "whitesmoke",
+    dark: "black",
+  };
+  return (
+    <button
+      style={{
+        backgroundColor: bgColor[type],
+        color: type === "light" ? "black" : "white",
+        fontWeight: "bold",
+        borderRadius: "5px",
+        padding: "3px",
+      }}
+      className={tailwind}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
